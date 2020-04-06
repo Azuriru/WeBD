4346ed27b2e5e752476a79ebdf2fce5aaa25b940
GameModerator <jeannelee823@gmail.com>
2020-04-06 18:33:37 +0800

    huge commit lol idk "stuff" fix, patch, style??

--- static/js/discord.js
+++ static/js/discord.js
@@ -5,13 +5,27 @@ const guildNav = build.div({
         html: guildList.map(guild => {
             return build.div({
                 class: 'bd-listItem',
-                html: build.div({
-                    class: 'bd-guildWrapper',
-                    html: build.div({
-                        class: guild.class ? guild.class : 'bd-guild',
-                        html: guild.name
+                html: [
+                    build.div({
+                        class: 'bd-pillWrapper',
+                        html: build.div({
+                            class: guild.pill ? `${guild.pill + ' bd-pill'}` : 'bd-pill'
+                        })
+                    }), 
+                    build.div({
+                        class: 'bd-guildWrapper',
+                        html: !guild.icon
+                        ? build.div({
+                            class: guild.class ? guild.class : 'bd-guild',
+                            html: guild.name
+                        })
+                        : build.img({
+                            class: 'bd-img bd-guild',
+                            src: `./img/avatars/${guild.icon}`
+                            // src: user.icon ? `${'./img/users' + user.icon}` : './img/users/placeholder.png'
+                        })
                     })
-                })
+                ]
             });
         })
     })
@@ -24,60 +38,71 @@ const guildSeparator = build.div({
     })
 });
 
-const privateChannels = build.div({
-    class: 'bd-privateChannels',
-    html:[
-        build.div({
-            class: 'bd-searchBar',
+const privateSearch = build.div({
+    class: 'bd-searchBar',
+    html: build.div({
+        class: 'bd-searchInner',
+        html: 'Find or start a conversation'
+    })
+});
+
+const privateList = build.div({
+    class: 'bd-scroller bd-hidden',
+    html: dmList.map(user => {
+        return user.header 
+        ? build.div({
+            class: 'bd-header',
             html: build.div({
-                class: 'bd-searchInner',
-                html: 'Find or start a conversation'
-            })
-        }),
-        build.div({
-            class: 'bd-scroller bd-hidden',
-            html: dmList.map(user => {
-                return build.div({
-                    class: 'bd-channel',
-                    html: [
-                        build.div({
-                            class: 'bd-avatar',
-                            html: build.img({
-                                class: 'bd-img',
-                                src: user.icon
-                                    ? `./img/${user.icon}`
-                                    : `./img/placeholder.png`
-                                // src: user.icon ? `${'./img/users' + user.icon}` : './img/users/placeholder.png'
-                            })
-                        }),
-                        build.div({
-                            class: 'bd-content',
-                            html: [
-                                build.div({
-                                    class: 'bd-username',
-                                    html: user.name
-                                }),
-                                build.div({
-                                    class: 'bd-status',
-                                    if: user.status,
-                                    html: user.status && build.span({
-                                        class: 'bd-status-text',
-                                        html: [
-                                            build.text(`${user.status.type} `),
-                                            build.span({
-                                                class: 'bd-status-name',
-                                                html: user.status.name
-                                            })
-                                        ]
-                                    })
-                                })
-                            ]
-                        })
-                    ]
-                });
+                class: 'bd-headerText',
+                html: user.header
             })
         })
-    ]
+        : build.div({
+            class: 'bd-channel',
+            html: [
+                build.div({
+                    class: 'bd-avatar',
+                    html: user.icon.indexOf('svg') > -1
+                        ? user.icon
+                        : build.img({
+                            class: 'bd-img',
+                            src: user.icon
+                                ? `./img/avatars/${user.icon}`
+                                : `./img/avatars/placeholder.png`,
+                            // src: user.icon ? `${'./img/users' + user.icon}` : './img/users/placeholder.png'
+                        })
+                }),
+                build.div({
+                    class: 'bd-content',
+                    html: [
+                        build.div({
+                            class: 'bd-username',
+                            html: user.name
+                        }),
+                        build.div({
+                            class: 'bd-status',
+                            if: user.status,
+                            html: user.status && build.span({
+                                class: 'bd-status-text',
+                                html: [
+                                    build.text(`${user.status.type} `),
+                                    build.span({
+                                        class: 'bd-status-name',
+                                        html: user.status.name
+                                    })
+                                ]
+                            })
+                        })
+                    ]
+                })
+            ]
+        });
+    })
+});
+
+const privateChannels = build.div({
+    class: 'bd-privateChannels',
+    html: [privateSearch, privateList]
 });
 
 const voiceBox = build.div({
