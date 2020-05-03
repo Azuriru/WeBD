2864787b9b67f71dea367966c083ba21a96f94c2
GameModerator <jeannelee823@gmail.com>
2020-05-04 00:42:32 +0800

    **commits**

--- static/js/quickpicker.js
+++ static/js/quickpicker.js
@@ -1,7 +1,7 @@
 const buildInput = v => {
-    return build.div({
+    return build.input({
         class: 'bd-input',
-        text: v.value
+        value: v.value
     });
 }
 
@@ -12,21 +12,91 @@ const buildColorValue = v => {
     })
 }
 
+// const pickerTextTitle = 
+const pickerText = build.div({
+    class: 'bd-pickerText',
+    children: [
+        build.div({
+            class: 'bd-pickerTitle',
+            text: 'Background'
+        }),
+        build.div({
+            class: 'bd-pickerSetWrapper',
+            children: pickerTextValues.map(v => {
+                return build.div({
+                    class: 'bd-pickerSet',
+                    children: [
+                        build.div({
+                            class: 'bd-pickerName',
+                            children: [
+                                build.text(v.name),
+                                build.span({
+                                    class: 'material-icons',
+                                    text: 'help',
+                                    title: v.tip
+                                })
+                            ]
+                        }),
+                        build.div({
+                            class: 'bd-pickerValue',
+                            children: [
+                                buildInput(v),
+                                build.span({
+                                    class: 'material-icons',
+                                    text: 'edit',
+                                })
+                            ]
+                        })
+                    ]
+                })
+            })
+        })
+    ]
+})
+
+const pickerColors = build.div({
+    class: 'bd-pickerColors',
+    children: [
+        build.div({
+            class: 'bd-pickerTitle',
+            text: 'Colors'
+        }),
+        build.div({
+            class: 'bd-pickerSetWrapper',
+            children: pickerValues.map(v => {
+                return build.div({
+                    class: 'bd-pickerSet',
+                    children: [
+                        build.div({
+                            class: 'bd-pickerName',
+                            text: v.name
+                        }),
+                        build.div({
+                            class: 'bd-pickerValue',
+                            child: buildColorValue(v)
+                        })
+                    ]
+                })
+            })
+        })
+    ]
+})
+
 const picker = build.div({
     class: 'bd-picker',
-    children: pickerValues.map(v => {
-        return build.div({
-            class: 'bd-pickerSet',
-            children: [
-                build.div({
-                    class: 'bd-pickerName',
-                    text: v.name
-                }),
-                build.div({
-                    class: 'bd-pickerValue',
-                    child: buildColorValue(v)
-                })
-            ]
-        })
-    })
+    children: [pickerText, pickerColors]
+});
+
+const resize = () => {
+    let width = window.innerWidth * .6,
+        scale = `.${width.toString().substring(0, 2)}`,
+        root = document.documentElement;
+
+    root.style.setProperty('--appMountScale', scale);
+}
+
+resize();
+
+window.addEventListener('resize', function() {
+    resize();
 });
